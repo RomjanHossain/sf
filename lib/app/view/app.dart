04bf35b90582aa7979bd0806205f5afe31b5a95f
RomjanHossain <romjan1412@proton.me>
2024-12-26 12:36:23 +0600

    base app

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:steadfast_task/counter/counter.dart';
-import 'package:steadfast_task/l10n/l10n.dart';
+import 'package:steadfast_task/presentation/pages/weather/view/weather_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,6 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // themeMode: ThemeMode.light,
       theme: ThemeData(
         fontFamily: 'CircularStd',
         appBarTheme: AppBarTheme(
@@ -15,9 +15,9 @@ class App extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      // localizationsDelegates: AppLocalizations.localizationsDelegates,
+      // supportedLocales: AppLocalizations.supportedLocales,
+      home: const WeatherPage(),
     );
   }
 }
